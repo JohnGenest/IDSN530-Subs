63fd9794b5732f3e7070d9fa4c03d1c04279e2e2
JohnGenest <73373019+JohnGenest@users.noreply.github.com>
2024-12-22 20:17:56 -0500

    Delete as1c directory

--- as1c/includes/nav.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <a href="index.php">HOME</a>
-    <a href="thebdb.php">THE BDB</a>
-    <a href="thebouts.php">THE BOUTS</a>
-    <a href="oldmilfod.php">OLD MIL</a>
-    <a href="resources.php">RESOURCES</a>
-</nav>
